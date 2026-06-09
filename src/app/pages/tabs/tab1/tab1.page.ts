d994a987ebdfe3d904f4990655d8c4081c220ad9
Michael.Guarino <michael.guarino@entaingroup.com>
2026-06-09 16:40:57 +0200

    First styling with placeholder data

--- src/app/pages/tabs/tab1/tab1.page.ts
+++ src/app/pages/tabs/tab1/tab1.page.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonContent, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { trendingDownOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonContent, IonIcon],
 })
 export class Tab1Page {
-  constructor() {}
+  constructor() {
+    addIcons({ trendingDownOutline });
+  }
 }
